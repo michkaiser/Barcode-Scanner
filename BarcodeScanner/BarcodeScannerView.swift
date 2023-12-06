f37e4657ec3d115b86de995703355a2224cc677c
michkaiser <michkaiser13@gmail.com>
2023-12-06 18:56:59 +0300

    changed not yet scanned to lowercase

---
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  BarcodeScanner
+//
+//  Created by Kaiser on 06/12/2023.
+//
+
+import SwiftUI
+
+struct AlertItem: Identifiable{
+    let id = UUID()
+    let title: String
+    let message: String
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext{
+    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input",
+                                              message: "Something is wrong with the camera. We are unable to capture the input.",
+                                              dismissButton: .default(Text("OK")))
+    
+    static let invalidScannedType = AlertItem(title: "Invalid Scan Type",
+                                              message: "The value scanned is not valid. This app scans EAN-8 and EAN-13.",
+                                              dismissButton: .default(Text("OK")))
+}
+
+struct BarcodeScannerView: View {
+    
+    @State private var scannedCode = ""
+    @State private var alertItem: AlertItem?
+    
+    var body: some View {
+        NavigationView{
+            VStack{
+                ScannerView(scannedCode: $scannedCode, alertItem: $alertItem)
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                Spacer().frame(height: 60)
+                
+                Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                Text(scannedCode.isEmpty ? "Not yet scanned" : scannedCode)
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(scannedCode.isEmpty ? .red : .green)
+                    .padding()
+               
+                
+            }
+            .navigationTitle("Barcode Scanner")
+            .alert(item : $alertItem) { alertItem in
+                Alert (title: Text(alertItem.title),
+                      message: Text(alertItem.message),
+                      dismissButton: alertItem.dismissButton)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarcodeScannerView()
+    }
+}
